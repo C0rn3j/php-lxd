b05d19ccedfd3f4e133defbd9213d92a44bdc5e8
Martin Rys <martin@rys.pw>
2024-02-19 19:05:52 +0100

    Add new exec-output log endpoints (5.15)

--- src/Endpoint/Containers/Logs.php
+++ src/Endpoint/Containers/Logs.php
@@ -21,7 +21,7 @@ class Logs extends AbstructEndpoint
     {
         $logs = [];
 
-        foreach ($this->get($this->getEndpoint().$name.'/logs/') as $log) {
+        foreach ($this->get($this->getEndpoint().$name.'/logs') as $log) {
             $logs[] = str_replace(
                 '/'.$this->client->getApiVersion().'/instances/'.$name.'/logs/',
                 '',
@@ -54,5 +54,50 @@ class Logs extends AbstructEndpoint
     public function remove($name, $log)
     {
         return $this->delete($this->getEndpoint().$name.'/logs/'.$log);
+    }
+
+    /**
+     * List of exec-output logs for a container
+     *
+     * @param  string $name Name of container
+     * @return array
+     */
+    public function allExecOutput($name)
+    {
+        $logs = [];
+
+        foreach ($this->get($this->getEndpoint().$name.'/logs/exec-output') as $log) {
+            $logs[] = str_replace(
+                '/'.$this->client->getApiVersion().'/instances/'.$name.'/logs/exec-output/',
+                '',
+                $log
+            );
+        }
+
+        return $logs;
+    }
+
+    /**
+     * Get the contents of a particular exec-output log file
+     *
+     * @param string $name  Name of container
+     * @param string $log   Name of log
+     * @return object
+     */
+    public function readExecOutput($name, $log)
+    {
+        return $this->get($this->getEndpoint().$name.'/logs/exec-output/'.$log);
+    }
+
+    /**
+     * Remove a particular exec-output log file
+     *
+     * @param string $name  Name of container
+     * @param string $log   Name of log
+     * @return object
+     */
+    public function removeExecOutput($name, $log)
+    {
+        return $this->delete($this->getEndpoint().$name.'/logs/exec-output/'.$log);
     }
 }
